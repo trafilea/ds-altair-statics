945ac8492f454f847cf5d2f8f08cf16e36290796
carlosflury <carlos.flury@trafilea.com>
2023-11-20 16:21:09 -0300

    altair/fixed members folder

--- js/old-home-2-body.js
+++ js/old-home-2-body.js
@@ -213,7 +213,7 @@ $('#btnCreateProject').click(function (e) {
         ad_id = res["ad_id"]
 
         // redirect to https://altair-trafilea.webflow.io/project-page with the project id
-        window.location.href = "https://api-integration-test-e340db.webflow.io/project-page?project_id=" + project_id + "&angle_id=" + angle_id + "&ad_id=" + ad_id;
+        window.location.href = "https://api-integration-test-e340db.webflow.io/members/project-page?project_id=" + project_id + "&angle_id=" + angle_id + "&ad_id=" + ad_id;
 
     })
     .fail((res) => {
