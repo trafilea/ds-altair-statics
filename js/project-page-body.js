923e75396b8b6d62dc7aac83eab98e6afa181942
Juan Sebastian Fernandez <juan.fernandez@trafilea.com>
2024-02-26 17:18:54 -0500

    Removing variables and adding logs to debug

--- js/project-page-body.js
+++ js/project-page-body.js
@@ -25,12 +25,10 @@ Webflow.push(function () {
             let benchmark_link = "<a href='" + link + "' target='_blank'>[Link]</a><br><br>"
             $("#txtBenchmarkComparison").html(benchmark_link + (benchmark_text).replaceAll("%%", "<br><br>"));
             // $("#txtBenchmarkComparison").html((benchmark_obj["hook"] + benchmark_obj["lead_structure"] + benchmark_obj["closing_cta"]).replaceAll("%%", "<br><br>").replaceAll("%%", "<br><br>"));
-
-            // Adding logic to enable/disable buttons after changing the benchmark selection
-            let able_to_regenerate = false;
-            let able_to_generate = true;
-            let able_to_save_draft = false;
-            let able_to_generate_storyboard = false;
+            console.log("able_to_generate: " + able_to_generate)
+            console.log("able_to_save_draft: " + able_to_save_draft)
+            console.log("able_to_regenerate: " + able_to_regenerate)    
+            console.log("able_to_generate_storyboard: " + able_to_generate_storyboard)  
             calculateButtons(able_to_regenerate, able_to_save_draft, able_to_generate, able_to_generate_storyboard)
         });
     });
