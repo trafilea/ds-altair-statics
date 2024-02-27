f532c5ec543544a0d18351134940d7f0fb7a5ff7
Juan Sebastian Fernandez <48601926+juansef14@users.noreply.github.com>
2024-02-27 10:04:14 -0500

    Feature/ds altair statics/adjustments in calculating buttons appeareance after changing benchmark (#13)
    * Adding change to save draft showing generate storyboard button
    
    * Adding changes to enable or disable buttons after changing benchmark
    
    * Removing variables and adding logs to debug
    
    * Removing logs

--- js/project-page-body.js
+++ js/project-page-body.js
@@ -25,6 +25,7 @@ Webflow.push(function () {
             let benchmark_link = "<a href='" + link + "' target='_blank'>[Link]</a><br><br>"
             $("#txtBenchmarkComparison").html(benchmark_link + (benchmark_text).replaceAll("%%", "<br><br>"));
             // $("#txtBenchmarkComparison").html((benchmark_obj["hook"] + benchmark_obj["lead_structure"] + benchmark_obj["closing_cta"]).replaceAll("%%", "<br><br>").replaceAll("%%", "<br><br>"));
+            calculateButtons(able_to_regenerate, able_to_save_draft, able_to_generate, able_to_generate_storyboard);
         });
     });
 
